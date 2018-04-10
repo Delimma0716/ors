c674340962ab0c4b612b9d511cdf6afffce4bb86
赵子卉 <zihui.zhao@iqunxing.com>
2018-04-10 17:04:48 +0800

    首页重定向到登录

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,10 @@
 
 <script>
 export default {
-  name: 'App',
-  mounted(){
-    // 已登陆过直接进入首页
-    if(localStorage.getItem('user_account')){
-      this.$router.push('apply')
-    }else if(localStorage.getItem('en_account')){
-      this.$router.push('enterprise/recruit')
-    }else{
-      this.$router.push('login')
-    }
-  }
+  name: 'App'
 }
 </script>
 
 <style lang="less" scoped>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,7 @@ export default new Router({
       path: '/',
       name: '首页',
       component: Index,
+      redirect: '/login',
       children: [
         {
           path: 'settings',
